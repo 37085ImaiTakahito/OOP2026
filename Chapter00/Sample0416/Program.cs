8386e6017ae9295513b67593d8c95546fbe429e3
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-04-16 11:56:24 +0900

    for文をwhile文に変更

--- Chapter00/Sample0416/Program.cs
+++ Chapter00/Sample0416/Program.cs
@@ -4,11 +4,12 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            int j = 0;
+            while (j < 10)
             {
                 Console.WriteLine("Hello, World!");
+                j++;
             }
         }
-
     }
 }
