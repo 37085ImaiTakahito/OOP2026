1fbb5ed4728400f7c4665b00d06f1b8193a1304d
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-04-23 10:30:47 +0900

    readonlyを実装

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -6,17 +6,18 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter 
 {
-    public class FeetConverter
+    public static class FeetConverter
     {
+        private static readonly double ratio = 0.3048;
         //フィートからメートルを求める
         public static double FromMeter(double meter)
         {
-            return meter / 0.3048;
+            return meter / ratio;
         }
         //メートルからフィートを求める
         public static double ToMeter(double feet)
         {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
