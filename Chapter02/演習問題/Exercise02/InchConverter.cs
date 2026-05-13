df8b331d4f3d2a08706b4486d0c38aedf2178224
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-05-13 11:19:57 +0900

    インチをメーターに変換

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class InchConverter {
+        private static readonly double ratio = 0.3048;
+        
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        
+        public static double ToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            PrintInchToMeterList(1, 10);
+        }
+
+        private static void PrintInchToMeterList(int start,int end) {
+            for (int inch = start; inch <= end; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine($"{inch}ft = {meter:0.0000}m");
+            }
         }
     }
 }
