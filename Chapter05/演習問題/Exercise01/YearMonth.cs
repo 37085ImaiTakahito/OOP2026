fffa169dbd4d2ec91249aab4303340f3f99f87dd
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-04 09:52:42 +0900

    演習5.1完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -8,20 +8,26 @@ namespace Exercise01 {
     //5.1.1
     public class YearMonth {
         public int Year { get; init; }
-        public bool Month { get; init; }
-        public bool Is21Century => 2001 <= 2100;
+        public int Month { get; init; }
 
-        public YearMonth(int year, bool month,bool is21century) {
+        public YearMonth(int year, int month ) {
             Year = year;
             Month = month;
-            Is21Century = is21century;
         }
 
-
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
 
         public YearMonth AddOneMonth() {
-
+            if(Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            }else {
+                return new YearMonth(Year, Month + 1);
+            }
         }
+
+        public override string ToString() => Year + "年" + Month + "月";
+            
+        
         
     }
 }
