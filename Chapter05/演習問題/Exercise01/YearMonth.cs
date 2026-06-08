d27ebc5a8df765dfffdea8a03a74151bcff26a98
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-08 11:18:37 +0900

    Exercise03終了

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     //5.1.1
     public class YearMonth {
+        //プロパティ(P114参照)
         public int Year { get; init; }
         public int Month { get; init; }
 
