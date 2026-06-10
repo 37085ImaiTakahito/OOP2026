8ed63b7f3241e153f2330e03a15c1152dcf27cdc
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-10 10:34:41 +0900

    6.2完了

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("1:");
+            var str1 = Console.ReadLine();
+            int num;
+            int.TryParse(str1,out num);
+            var str2 = num.ToString("N0");
+            Console.WriteLine(str2);
+        }
+    }
+}
