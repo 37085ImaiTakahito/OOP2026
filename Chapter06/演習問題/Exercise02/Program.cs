e480cdf8d74df21d8b51c8a06d53b6a93fc3e5cf
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-10 10:39:42 +0900

    6.2完了

--- Chapter06/演習問題/Exercise02/Program.cs
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -5,7 +5,7 @@
             var str1 = Console.ReadLine();
             int num;
             int.TryParse(str1,out num);
-            var str2 = num.ToString("N0");
+            var str2 = num.ToString("#,0");
             Console.WriteLine(str2);
         }
     }
