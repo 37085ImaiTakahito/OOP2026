1930a4bb4b6d5b6b9ae9489632522b057f8185e7
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-10 10:01:21 +0900

    6.1完了

---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("1:");
+            var str1 = Console.ReadLine();
+            Console.Write("2:");
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            }
+            else {
+                Console.WriteLine("等しくない");
+            }
+
+        }
+    }
+}
