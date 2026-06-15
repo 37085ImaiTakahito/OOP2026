5f3150d1344da9487fefacdc1ce7e171b0f97300
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-15 10:45:04 +0900

    文字列の連結と分割　完了

--- Chapter06/文字列の連結と分割/Section05/Program.cs
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace Section05 {
+﻿using System.Text;
+
+namespace Section05 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var sb = new StringBuilder();
+            foreach(var word in GetWords()) {
+                sb.Append(word);
+            }
+            Console.WriteLine(sb);
+
+            var result = String.Join(",", GetWords());
+            Console.WriteLine(result);
+
+        }
+
+        private static IEnumerable<object> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
         }
     }
 }
