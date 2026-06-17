eecfd15eb620528349711b8f81210f671f1298be
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-17 12:04:47 +0900

    同期

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+
+            var books = Books.GetBooks();
+            var priceAverage = books.Average(x => x.Price);
+            var pageSum = books.Sum(x => x.Pages);
+            var MaxPrice = books.Max(x => x.Price);
+
+            Console.WriteLine("平均金額：" + priceAverage);
+            Console.WriteLine("合計ページ：" + pageSum);
+            Console.WriteLine("高価な本：" + MaxPrice);
+
+
+            Console.WriteLine("--- 500円以上の本 ---");
+            foreach(var book in books.Where(x => x.Price >= 500)) {
+                Console.WriteLine(book.Title);
+            }
+
+            //250ページ以上の本を上位３冊出力
+            foreach (var book in books.Where(x => x.Pages >= 250).Take(3)) {
+                    Console.WriteLine(book.Title);
+            }
+
+        }
+    }
+}
