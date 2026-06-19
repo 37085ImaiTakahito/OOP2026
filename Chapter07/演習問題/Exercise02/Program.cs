c13caa5531b67ed513891cd454e75a544c85e19d
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-19 12:04:16 +0900

    同期

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -43,11 +43,13 @@ namespace Exercise02 {
         }
 
         private static void Exercise2(List<Book> books) {
-            
+            var count = books.Count(x => x.Title.Contains("C#"));
+            Console.WriteLine(count);
         }
 
         private static void Exercise3(List<Book> books) {
-            
+            var count = books.Count(x => x.Title.Contains("C#"));
+            Console.WriteLine(count);
         }
 
         private static void Exercise4(List<Book> books) {
