916c3b1affad561960e1eb78e9c3440e6278d2ad
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-22 10:31:41 +0900

    7.2.7 完了

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -69,7 +69,9 @@ namespace Exercise02 {
         }
 
         private static void Exercise7(List<Book> books) {
-            
+            foreach (var book in books.Where(x => x.Title.Contains("C#") && x.Pages <= 500)) {
+                Console.WriteLine($"{book.Title}");
+            }
         }
     }
 }
