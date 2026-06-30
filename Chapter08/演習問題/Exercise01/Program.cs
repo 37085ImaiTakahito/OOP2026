7ef6e2920a1b4c81fa7391c521abb0d8531aa63b
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-06-30 14:50:48 +0900

    問題8.1.2完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -4,8 +4,10 @@ namespace Exercise01 {
         static void Main(string[] args) {
             var text = "Cozy lummox gives smart squid who asks for job pen";
 
+            Console.WriteLine("問題8.1.1");
             Exercise1(text);
             Console.WriteLine();
+            Console.WriteLine("問題8.1.2");
             Exercise2(text);
             Console.WriteLine();
         }
@@ -26,13 +28,24 @@ namespace Exercise01 {
             foreach (var d in dict.OrderBy(x => x.Key)) {
                 Console.WriteLine($"{d.Key} {d.Value}");
             }
-
         }
 
         private static void Exercise2(string text) {
-            //コミットコメント（問題８．2.１完成）
-
-
+            //コミットコメント（問題8.1.2完成）
+            var dict = new SortedDictionary<char, int>();
+            foreach (char c in text.ToUpper()) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    }
+                    else {
+                        dict[c] = 1;
+                    }
+                }
+            }
+            foreach (var d in dict) {
+                Console.WriteLine($"{d.Key} {d.Value}");
+            }
         }
     }
 }
