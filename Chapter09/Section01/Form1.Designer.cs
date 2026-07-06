3ff39fc7f9d90acf76d6da7626d37d9e4d86e3cd
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-07-06 14:14:27 +0900

    P198まで

--- Chapter09/Section01/Form1.Designer.cs
+++ Chapter09/Section01/Form1.Designer.cs
@@ -23,113 +23,53 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            label1 = new Label();
-            ボタン = new Button();
-            tbOutput = new TextBox();
-            nudNum1 = new NumericUpDown();
-            nudNum2 = new NumericUpDown();
-            pbBox = new PictureBox();
-            ボタン2 = new Button();
-            ofdOpen = new OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)nudNum1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)nudNum2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pbBox).BeginInit();
+            button1 = new Button();
+            dtpDate = new DateTimePicker();
+            tbOut = new TextBox();
             SuspendLayout();
             // 
-            // label1
+            // button1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(60, 39);
-            label1.Name = "label1";
-            label1.Size = new Size(60, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Chapter09";
+            button1.Location = new Point(276, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 40);
+            button1.TabIndex = 0;
+            button1.Text = "取得";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
-            // ボタン
+            // dtpDate
             // 
-            ボタン.Location = new Point(38, 134);
-            ボタン.Name = "ボタン";
-            ボタン.Size = new Size(214, 105);
-            ボタン.TabIndex = 1;
-            ボタン.Text = "ボタン";
-            ボタン.UseVisualStyleBackColor = true;
-            ボタン.Click += ボタン_Click;
+            dtpDate.Location = new Point(32, 34);
+            dtpDate.Name = "dtpDate";
+            dtpDate.Size = new Size(200, 23);
+            dtpDate.TabIndex = 1;
             // 
-            // tbOutput
+            // tbOut
             // 
-            tbOutput.Location = new Point(38, 271);
-            tbOutput.Name = "tbOutput";
-            tbOutput.Size = new Size(310, 23);
-            tbOutput.TabIndex = 2;
-            // 
-            // nudNum1
-            // 
-            nudNum1.Location = new Point(38, 84);
-            nudNum1.Name = "nudNum1";
-            nudNum1.Size = new Size(120, 23);
-            nudNum1.TabIndex = 3;
-            // 
-            // nudNum2
-            // 
-            nudNum2.Location = new Point(228, 84);
-            nudNum2.Name = "nudNum2";
-            nudNum2.Size = new Size(120, 23);
-            nudNum2.TabIndex = 3;
-            // 
-            // pbBox
-            // 
-            pbBox.Image = Properties.Resources.losSanta_Monica_3;
-            pbBox.Location = new Point(418, 113);
-            pbBox.Name = "pbBox";
-            pbBox.Size = new Size(314, 246);
-            pbBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            pbBox.TabIndex = 4;
-            pbBox.TabStop = false;
-            // 
-            // ボタン2
-            // 
-            ボタン2.Location = new Point(454, 84);
-            ボタン2.Name = "ボタン2";
-            ボタン2.Size = new Size(75, 23);
-            ボタン2.TabIndex = 5;
-            ボタン2.Text = "開く...";
-            ボタン2.UseVisualStyleBackColor = true;
-            // 
-            // ofdOpen
-            // 
-            ofdOpen.FileName = "openFileDialog1";
-            ofdOpen.FileOk += ofdOpen_FileOk;
+            tbOut.Location = new Point(33, 104);
+            tbOut.Name = "tbOut";
+            tbOut.Size = new Size(199, 23);
+            tbOut.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(ボタン2);
-            Controls.Add(pbBox);
-            Controls.Add(nudNum2);
-            Controls.Add(nudNum1);
-            Controls.Add(tbOutput);
-            Controls.Add(ボタン);
-            Controls.Add(label1);
+            Controls.Add(tbOut);
+            Controls.Add(dtpDate);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)nudNum1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)nudNum2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pbBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Button ボタン;
-        private TextBox tbOutput;
-        private NumericUpDown nudNum1;
-        private NumericUpDown nudNum2;
-        private PictureBox pbBox;
-        private Button ボタン2;
-        private OpenFileDialog ofdOpen;
+        private Button button1;
+        private DateTimePicker dtpDate;
+        private TextBox tbOut;
     }
 }
--- Chapter09/Section01/Form1.cs
+++ Chapter09/Section01/Form1.cs
@@ -4,14 +4,42 @@ namespace Section01 {
             InitializeComponent();
         }
 
-        private void ƒ{ƒ^ƒ“_Click(object sender, EventArgs e) {
-            var num = nudNum1.Value + nudNum2.Value;
-            tbOutput.Text = num.ToString();
+        private void button1_Click(object sender, EventArgs e) {
+            DateTime dt1 = dtpDate.Value;
+            //DayOfWeek dayOfWeek = dt1.DayOfWeek;
+
+            if (DateTime.IsLeapYear(dt1.Year)) {
+                tbOut.Text = "‚¤‚é‚¤”N‚Å‚·";
+            }
+            else {
+                tbOut.Text = "‚¤‚é‚¤”N‚Å‚Í‚ ‚è‚Ü‚¹‚ñ";
+            }
+
+            //switch (dayOfWeek) {
+            //    case DayOfWeek.Saturday:
+            //        tbOut.Text = "¡“ú‚Í“y—j“ú‚Å‚·";
+            //        break;
+            //    case DayOfWeek.Sunday:
+            //        tbOut.Text = "¡“ú‚Í“ú—j“ú‚Å‚·";
+            //        break;
+            //   case DayOfWeek.Monday:
+            //        tbOut.Text = "¡“ú‚ÍŒ—j“ú‚Å‚·";
+            //        break;
+            //    case DayOfWeek.Tuesday:
+            //        tbOut.Text = "¡“ú‚Í‰Î—j“ú‚Å‚·";
+            //        break;
+            //    case DayOfWeek.Wednesday:
+            //        tbOut.Text = "¡“ú‚Í…—j“ú‚Å‚·";
+            //        break;
+            //    case DayOfWeek.Thursday:
+            //        tbOut.Text = "¡“ú‚Í–Ø—j“ú‚Å‚·";
+            //        break;
+            //   case DayOfWeek.Friday:
+            //        tbOut.Text = "¡“ú‚Í‹à—j“ú‚Å‚·";
+            //        break;
+            //}
+
         }
 
-        private void ofdOpen_FileOk(object sender, System.ComponentModel.CancelEventArgs e) {
-            ofdOpen.ShowDialog();
-            
-        }
     }
 }
