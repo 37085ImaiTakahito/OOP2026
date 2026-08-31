1ef4b9961691d8582a9be346c21f662a87436d9a
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-08-31 14:39:42 +0900

    Databaseクラス作成 テストOK

--- FormApps/SQLiteProductSample/Program.cs
+++ FormApps/SQLiteProductSample/Program.cs
@@ -9,6 +9,9 @@ internal static class Program
 
         try
         {
+            //SQLiteデータベースを初期化する
+            //products.dbが存在しない場合は作成され
+            //Productsテーブルも存在しない場合だけ生成される
             Database.Initialize();
             Application.Run(new Form1());
         }
