7f43c2962e2178d63b50d0a7f1d82174027815a0
崇仁 今井 <oibc37085@ojs.ac.jp>
2026-09-08 14:13:52 +0900

    途中経過

--- FormApps/CarReportSystem/CarReportRepository.cs
+++ FormApps/CarReportSystem/CarReportRepository.cs
@@ -1,10 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//
+//
 namespace CarReportSystem {
     internal class CarReportRepository {
+        //全レポートを取得する。Read(SELECT)に相当する
+        public List<CarReport> GetAll() {
+
+            var reports = new List<CarReport>();
+
+            using var connection = Database.GetConnection();
+            connection.Open();
+
+            //SQLを実行するためのコマンドオブジェクトを作る
+            using var command = connection.CreateCommand();
+
+            //テーブルを作るSQL
+            command.CommandText =
+                """
+            SELECT 
+                Id,Date,Auther,Maker,CarName,Report,Picture
+            FROM CarReport
+            ORDER BY Id;
+            """;
+
+            //SELECTを実行し、複数行の検索結果を読み取る
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read()) {
+                reports .Add(new CarReport {
+                    Id = reader.GetInt32(0),  
+                    Date = DateTime.ParseExact(
+                        reader.GetString(1),
+                        "yyyy-MM-dd",
+                        CultureInfo.InvariantCulture),
+                    Author = reader.GetString(2),
+                    Maker = (CarReport.MakerGroup)reader.GetInt32(3),
+                    CarName = reader.GetString(4),
+                    Report = reader.GetString(5),
+                    Picture = reader.IsDBNull(6) 
+                        ? null :  BytesToImage(reader.GetFieldValue<byte[]>(6))
+                });
+            }
+            return reports;
+        }
+
+        //商品を1件追加する。Create(INSERT)に相当する
+        //戻り値として自動採番されたIdを返す
+        public int Add(string name, int price) {
+            //接続オブジェクトを生成する
+            using var connection = Database.GetConnection();
+
+            //DBを開く
+            connection.Open();
+
+            //SQLを実行するためのコマンドオブジェクトを作る
+            using var command = connection.CreateCommand();
+
+            command.CommandText =
+                """
+            INSERT INTO Products (Name, Price)
+            VALUES ($name,$price);
+
+            SELECT last_insert_rowid();
+            """;
+
+            command.Parameters.AddWithValue("$name", name);
+            command.Parameters.AddWithValue("$price", price);
+
+
+            //一つの値を返すSQLを実行する
+            var result = command.ExecuteScalar();
+
+            if (result is null)
+                throw new InvalidOperationException("登録した商品のIDを取得できませんでした");
+
+            //SQLiteのINTEGERはlongとして返るため、intへ変換する
+            return Convert.ToInt32((long)result);
+        }
+
+        public void Update(CarReport report) {
+            //接続オブジェクトを生成する
+            using var connection = Database.GetConnection();
+            connection.Open();
+            using var command = connection.CreateCommand();
+
+            command.CommandText =
+                """
+            UPDATE CarReports
+            SET Date = $date, Author = $author, Maker = $maker,
+                CarName = $carName, Report = $report, Picture = $picture
+            WHERE Id = $id;
+            """;
+
+            command.Parameters.AddWithValue("$date", report.Date);
+            command.Parameters.AddWithValue("$author", report.Author);
+            command.Parameters.AddWithValue("$maker", report.Maker);
+            command.Parameters.AddWithValue("$carName", report.CarName);
+            command.Parameters.AddWithValue("$report", report.Report);
+            command.Parameters.AddWithValue("$picture", report.Picture);
+            command.Parameters.AddWithValue("id", report.Id);
+
+            //更新件数が0なら対象が存在しない
+            if (command.ExecuteNonQuery() == 0)
+                throw new InvalidOperationException("修正対象の商品が見つかりませんでした。");
+        }
+
+        public void Delete(int id) {
+            using var connection = Database.GetConnection();
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText =
+                """
+            DELETE FROM Products
+            WHERE  Id = $id;
+            """;
+
+            command.Parameters.AddWithValue("id", id);
+            command.ExecuteNonQuery();
+        }
+
+        private static byte[]? ImageToBytes(Image? image) {
+            if (image is null) return null;
+
+            using var stream = new MemoryStream();
+            image.Save(stream, image.RawFormat);
+            return stream.ToArray();
+        }
     }
 }
--- FormApps/CarReportSystem/Database.cs
+++ FormApps/CarReportSystem/Database.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,47 @@ using System.Threading.Tasks;
 
 namespace CarReportSystem {
     internal class Database {
+        //DBファイルの保存場所
+        private static readonly string DatabasePath =
+            Path.Combine(AppContext.BaseDirectory, "carreport.db");
+
+        //SQLiteへ接続するための接続文字列
+        private static readonly string ConnectionString =
+            $"Data Source={DatabasePath}";
+
+        //DBファイルの保存場所を外部から確認するための読み取り専用プロパティ
+        public static string FilePath => DatabasePath;
+
+        //新しいSQLiteConnectionを生成して返す
+        public static SqliteConnection GetConnection() {
+            return new SqliteConnection(ConnectionString);
+        }
+
+        //DBの初期化処理
+        public static void Initialize() {
+            //接続オブジェクトを生成する
+            using var connection = GetConnection();
+
+            //DBを開く
+            connection.Open();
+
+            //SQLを実行するためのコマンドオブジェクトを作る
+            using var command = connection.CreateCommand();
+
+            //Productテーブルを作るSQL
+            //IF NOT EXISTSにより、既にテーブルがあってもエラーにならない
+            command.CommandText =
+                """
+            CREATE TABLE IF NOT EXISTS Products(
+                Id      INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name    TEXT NOT NULL,
+                Price   INTEGER NOT NULL CHECK (Price >= 0)
+            );
+            """;
+
+            //結果行を返さないSQLを実行する
+            command.ExecuteNonQuery();
+        }
     }
 }
+
